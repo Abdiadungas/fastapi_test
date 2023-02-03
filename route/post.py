762018617e75810fa18b5f73f44c39b298809955
ABDI ADUGNA <51967973+Abdiadungas@users.noreply.github.com>
2023-02-03 05:36:41 -0800

    Update post.py

--- route/post.py
+++ route/post.py
@@ -4,6 +4,6 @@ from config.db import conn
 from schemas.post import  serializeList
 user = APIRouter() 
 
-@user.get('/post')
+@user.get('/')
 async def find_all_users():
     return serializeList(conn.Job.camp.find())
